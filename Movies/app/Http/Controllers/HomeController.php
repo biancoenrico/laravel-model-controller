2e132531a429dc73f6cc0142887420f1cf11665f
enrico bianco <biancoenrico71@gmail.com>
2021-06-10 15:20:57 +0200

    query

--- Movies/app/Http/Controllers/HomeController.php
+++ Movies/app/Http/Controllers/HomeController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class HomeController extends Controller
 {
-    //
+    public function home(){
+        $data = [
+            'title'=> 'ciao'
+        ];
+        return view('home', $data);
+    }
+
+    public function movies(){
+
+        $movies = Movie::all();
+
+        dd($movies);
+        return view('movie');
+    }
 }
